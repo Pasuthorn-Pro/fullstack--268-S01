14a911a4d455455dc34593fd50f60567e0d3faf7
Pasuuu <pasuthorn1406@gmail.com>
2026-02-10 16:32:17 +0700

    asdfghj

---
+++ AxiousNodeHtml.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+var bodyParser = require('body-parser');
+
+const base_url = "http://localhost:3000";
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", async (req, res) => {
+    try {
+        const response = await axios.get(best_url + '/books');
+        res.render("books", {book: response.data });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error');
+    }
+});
+
